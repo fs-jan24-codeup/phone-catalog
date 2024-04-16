dbf189f790df994339fdca2d8fb65a28a79a19a2
Vadym <khromeicreate@gmail.com>
2024-04-16 13:12:42 +0300

    add router-with-phones-page

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './global.css';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <App />
-    </Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="phones" element={<div>Phone page</div>} />
+        </Route>
+      </Routes>
+    </Router> 
   </React.StrictMode>,
 );
