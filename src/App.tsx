55139e6306ce30c2f61c35de6abe4a1ba4a0cab6
Vasya564 <dudlavasil@gmail.com>
2024-04-17 13:38:30 +0300

    Add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Header } from './components/Header/Header';
+import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
 export const App: React.FC = () => {
