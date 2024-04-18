b22f6ef3fe56d4fc01cced96dcec82193e219c28
illya-onyshchuk <llayonys@gmail.com>
2024-04-18 09:51:14 +0300

    adaptive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+
 import { PageLayout } from './components/PageLayout';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 export const App: React.FC = () => {
   return (
     <>
-      <PageLayout />
+      <Header />
+      <PageLayout>
+        <Outlet />
+      </PageLayout>
+      <Footer />
     </>
   );
 };
--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,22 +1,16 @@
-import { Outlet } from 'react-router-dom';
-import { Footer } from '../Footer';
-import { Header } from '../Header';
 import './PageLayout.scss';
+import React from 'react';
 
-export const PageLayout = () => {
+type Props = {
+  children: React.ReactNode;
+};
+
+export const PageLayout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="grid_container">
-      <div className="header_layout header_layout--lg ">
-        <Header />
+    <>
+      <div className="grid_container">
+        <main className="main_layout">{children}</main>
       </div>
-      <main className="main_layout">
-        <div className="container">
-          <Outlet />
-        </div>
-      </main>
-      <div className="footer_layout footer_layout--lg ">
-        <Footer />
-      </div>
-    </div>
+    </>
   );
 };
