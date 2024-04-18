f66eb01a400fc8c7408dbffea9ec4c886de8d9c5
Vasya564 <dudlavasil@gmail.com>
2024-04-18 10:40:36 +0300

    Display phones on phone page

--- src/components/CardLayout/CardLayout.tsx
+++ src/components/CardLayout/CardLayout.tsx
@@ -1,85 +1,21 @@
-import fav from '../../assets/icons/favourites.svg'
+import favourites from '../../assets/icons/favourites.svg';
+import { Phone } from '../../types/Phone';
 import './CardLayout.scss';
 
-//DELETE THIS
-const TEST__PHONE = {
-  "id": "apple-iphone-11-128gb-black",
-  "category": "phones",
-  "namespaceId": "apple-iphone-11",
-  "name": "Apple iPhone 11 128GB Black",
-  "capacityAvailable": [
-      "64GB",
-      "128GB",
-      "256GB"
-  ],
-  "capacity": "128GB",
-  "priceRegular": 1100,
-  "priceDiscount": 1050,
-  "colorsAvailable": [
-      "black",
-      "green",
-      "yellow",
-      "white",
-      "purple",
-      "red"
-  ],
-  "color": "black",
-  "images": [
-      "img/phones/apple-iphone-11/black/00.webp",
-      "img/phones/apple-iphone-11/black/01.webp",
-      "img/phones/apple-iphone-11/black/02.webp",
-      "img/phones/apple-iphone-11/black/03.webp",
-      "img/phones/apple-iphone-11/black/04.webp"
-  ],
-  "description": [
-      {
-          "title": "And then there was Pro",
-          "text": [
-              "A transformative triple-camera system that adds tons of capability without complexity.",
-              "An unprecedented leap in battery life. And a mind-blowing chip that doubles down on machine learning and pushes the boundaries of what a smartphone can do. Welcome to the first iPhone powerful enough to be called Pro."
-          ]
-      },
-      {
-          "title": "Camera",
-          "text": [
-              "Meet the first triple-camera system to combine cutting-edge technology with the legendary simplicity of iPhone. Capture up to four times more scene. Get beautiful images in drastically lower light. Shoot the highest-quality video in a smartphone — then edit with the same tools you love for photos. You’ve never shot with anything like it."
-          ]
-      },
-      {
-          "title": "Shoot it. Flip it. Zoom it. Crop it. Cut it. Light it. Tweak it. Love it.",
-          "text": [
-              "iPhone 11 Pro lets you capture videos that are beautifully true to life, with greater detail and smoother motion. Epic processing power means it can shoot 4K video with extended dynamic range and cinematic video stabilization — all at 60 fps. You get more creative control, too, with four times more scene and powerful new editing tools to play with."
-          ]
-      }
-  ],
-  "screen": "6.1' IPS",
-  "resolution": "1792x828",
-  "processor": "Apple A13 Bionic",
-  "ram": "4GB",
-  "camera": "12 Mp + 12 Mp + 12MP",
-  "zoom": "Digital, 5x",
-  "cell": [
-      "GPRS",
-      "EDGE",
-      "WCDMA",
-      "UMTS",
-      "HSPA",
-      "LTE"
-  ]
-}
+type Props = {
+  good: Phone;
+};
 
-export const CardLayout = () => {
-   //CHANGE THIS
-  const { images, name, priceRegular, priceDiscount, ram, screen, capacity } = TEST__PHONE;
+export const CardLayout: React.FC<Props> = ({ good }) => {
+  const { images, name, priceRegular, priceDiscount, ram, screen, capacity } =
+    good;
 
   return (
     <article className="card">
       <div className="card__container">
         <img src={images[0]} alt={name} className="card__image" />
 
-        <div className="card__name">
-          {name}
-        </div>
+        <div className="card__name">{name}</div>
 
         <div className="card__prices">
           <div className="card__price--discount">{`${priceDiscount}$`}</div>
@@ -89,27 +25,31 @@ export const CardLayout = () => {
         <div className="card__breakline"></div>
 
         <div className="card__characteristics characteristics">
-          <div className='screen'>
-            <p className='characteristics--name'>Screen</p>
-            <p className='characteristics--value'>{screen}</p>
+          <div className="screen">
+            <p className="characteristics--name">Screen</p>
+            <p className="characteristics--value">{screen}</p>
           </div>
 
-          <div className='capacity'>
-            <p className='characteristics--name'>Capacity</p>
-            <p className='characteristics--value'>{capacity}</p>
+          <div className="capacity">
+            <p className="characteristics--name">Capacity</p>
+            <p className="characteristics--value">{capacity}</p>
           </div>
 
-          <div className='ram'>
-            <p className='characteristics--name'>RAM</p>
-            <p className='characteristics--value'>{ram}</p>
+          <div className="ram">
+            <p className="characteristics--name">RAM</p>
+            <p className="characteristics--value">{ram}</p>
           </div>
         </div>
 
         <div className="card__button card__button--container">
-          <button className="card__button--add">Add to card</button>
+          <button className="card__button--add">Add to cart</button>
 
           <button className="card__button--favourite">
-            <img src={fav} alt="Favourite" className='card__button--image'/>
+            <img
+              src={favourites}
+              alt="Favourite"
+              className="card__button--image"
+            />
           </button>
         </div>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 // import 'normalize.css';
 import './styles/styles.scss';
 import App from './App.tsx';
-import { PhonePages } from './pages/PhonesPage';
+import { PhonesPage } from './pages/PhonesPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<div>Home page</div>} />
-          <Route path="phones" element={<PhonePages />}>
+          <Route path="phones" element={<PhonesPage />}>
             <Route path=":phoneId?" element={<div>Phone details page</div>} />
           </Route>
           <Route path="tablets" element={<div>Tablets page</div>}>
