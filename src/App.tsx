b67bc202d31777f64a89c85be227a105cbfc9d71
Kateryna Shepetska <shepetskayaa@gmail.com>
2024-04-18 12:03:53 +0300

    cartitem inside cartpage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { PageLayout } from './components/PageLayout';
-import { CartItem } from './components/CartItem';
 
 export const App: React.FC = () => {
   return (
@@ -11,7 +10,6 @@ export const App: React.FC = () => {
       <Header />
       <PageLayout>
         <Outlet />
-        <CartItem />
       </PageLayout>
       
       <Footer />
