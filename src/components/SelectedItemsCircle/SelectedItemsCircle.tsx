e38e57e453c4af869a95fbc7b4c57b4be9f6a87b
Vitalii Vashkevych <NuclearSunStar@gmail.com>
2024-04-19 15:41:14 +0300

    Fixed counter

--- src/components/SelectedItemsCircle/SelectedItemsCircle.tsx
+++ src/components/SelectedItemsCircle/SelectedItemsCircle.tsx
@@ -2,13 +2,13 @@ import { useAppContext } from "../../hooks/useAppContext"
 import './SelectedItemsCircle.scss';
 
 export const SelectedItemsCircle = () => {
-  const { cart } = useAppContext();
+  const { cart, itemCount } = useAppContext();
 
   return (
     <>
       {!!cart.length && (
         <div className="circle">
-          <div className="circle--text">{cart.length}</div>
+          <div className="circle--text">{itemCount}</div>
         </div>
       )}
     </>
