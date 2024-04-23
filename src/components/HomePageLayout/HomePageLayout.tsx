27fd6bf9a29e0a49651ee1c95e19ccb37b97a1d6
ViktoriiaRepo <samarityankav@gmail.com>
2024-04-23 15:01:49 +0300

    Addes  new models slider and fetch data

--- src/components/HomePageLayout/HomePageLayout.tsx
+++ src/components/HomePageLayout/HomePageLayout.tsx
@@ -9,6 +9,7 @@ import { Product } from '../../types/Product';
 
 export const HomePageLayout: React.FC = () => {
   const [phonesWithHotPrices, setPhonesWithHotPrices] = useState<Product[]>([]);
+  const [newModels, setNewModels] = useState<Product[]>([]);
 
   useEffect(() => {
     getProducts('./api/phones.json')
@@ -19,13 +20,26 @@ export const HomePageLayout: React.FC = () => {
       })
       .catch(error => console.error('Error fetching phones:', error));
   }, []);
+
+  useEffect(() => {
+    getProducts('./api/phones.json')
+      .then(phones => {
+        phones.sort((a, b) => a.priceRegular - b.priceRegular);
+        const limitedPhones = phones.slice(0, 12);
+        setNewModels(limitedPhones);
+      })
+      .catch(error => console.error('Error fetching phones:', error));
+  }, []);
+
   return (
     <div className="home home__grid">
       <h1 className="home__title">Welcome to Nice Gadgets store!</h1>
       <div className="home__slider">
         <HomeSlider />
       </div>
-      <div className="home__brand-new-models">Brand new models</div>
+      <div className="home__brand-new-models">
+        <ProductSlider products={newModels} title="Brand new models" />
+      </div>
       <div className="home__shop-by-category">
         <CategoriesSection />
       </div>
---
+++ src/components/NewModels/NewModels.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NewModels: React.FC = ({}) => {
+  return (
+    <div>NewModels</div>
+  )
+}
