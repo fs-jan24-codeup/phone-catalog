1f5c81ff258d5902ce3e840b225f9502b68a597e
Kateryna Shepetska <shepetskayaa@gmail.com>
2024-04-23 19:25:55 +0300

    fixes

--- src/components/TechSpecs/TechSpecs.tsx
+++ src/components/TechSpecs/TechSpecs.tsx
@@ -6,36 +6,31 @@ type Props = {
 }
 
 export const TechSpecs: React.FC<Props> = ({ good }) => {
-  const keys: string[] = ['screen', 'resolution', 'processor', 'ram', 'camera', 'zoom'];
-  const cellKey: string = 'cell';
+
+  if (!good) return null;
+
+  const { screen, resolution, processor, ram, camera, zoom, cell } = good;
+
+  const specs: { [key: string]: string } = {
+    screen,
+    resolution,
+    processor,
+    ram,
+    camera,
+    zoom,
+    cell: cell.join(', '),
+  };
 
   return (
     <div className='specs'>
       <h1 className='specs__title line'>Tech specs</h1>
       <div className='specs__description'>
-        {good && (
-          <>
-            {Object.entries(good).map(([key, value]) => (
-              keys.includes(key) && (
-                <div className='specs__container' key={key}>
-                  <p className='specs_subtitle'>{key.charAt(0).toUpperCase() + key.slice(1)}</p>
-                  <p className='specs_text'>{value}</p>
-                </div>
-              )
-            ))}
-
-            {Object.entries(good).map(([key, value]) => (
-              key === cellKey && (
-                <div className='specs__container' key={key}>
-                  <p className='specs_subtitle'>{key.charAt(0).toUpperCase() + key.slice(1)}</p>
-                  <div className='specs_text cell'>
-                    {(value as string[]).join(', ')}
-                  </div>
-                </div>
-              )
-            ))}
-          </>
-        )}
+        {Object.entries(specs).map(([key, value]) => (
+          <div className='specs__container' key={key}>
+            <p className='specs_subtitle'>{key.charAt(0).toUpperCase() + key.slice(1)}</p>
+            <p className='specs_text'>{value}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
