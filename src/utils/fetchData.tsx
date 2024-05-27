979677eee4c9a2571e1f5e59a3151198b8ab20a6
Tetiana Lisne <tplovic@gmail.com>
2024-05-27 20:12:53 +0300

    fix fetch from home endpoint, add registerRequest to sign up

--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -1,6 +1,6 @@
-export function apiRequest(url: string) {
-  const baseUrl = import.meta.env.VITE_API_URL;
+const baseUrl = import.meta.env.VITE_API_URL;
 
+export function apiRequest(url: string) {
   return fetch(`${baseUrl}${url}`)
     .then(response => {
       if (!response.ok) {
@@ -12,4 +12,28 @@ export function apiRequest(url: string) {
       console.error('Error fetching products:', error);
       return [];
     });
+}
+
+export async function registerRequest(userData: {
+  email: string;
+  password: string;
+  name?: string;
+}) {
+  return fetch(`${baseUrl}/auth/registration`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .catch(error => {
+    console.error('Error fetching products:', error);
+    return [];
+  });
 }
