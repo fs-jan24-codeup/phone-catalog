b0da6c2eab4e9e2d963387d53824439490cd9cef
Tetiana Lisne <tplovic@gmail.com>
2024-05-28 20:44:40 +0300

    fix login

--- src/components/PrivateRoutes.tsx
+++ src/components/PrivateRoutes.tsx
@@ -6,6 +6,5 @@ import { Navigate } from 'react-router-dom';
 export const PrivateRoutes: React.FC = () => {
   const { isAuthenticated } = useAuth();
 
-  // return isAuthenticated ? element : (<Navigate to="/login"/>);
-  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />;
+  return isAuthenticated ? <Outlet /> : <Navigate to='/' />;
 };
